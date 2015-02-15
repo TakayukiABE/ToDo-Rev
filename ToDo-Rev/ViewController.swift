6a83a66396c64e843a5c92030d45f3c1ad10350a
TakayukiABE <s1411344@coins.tsukuba.ac.jp>
2015-02-15 22:21:54 +0900

    create table view on storyboard

--- ToDo-Rev/ViewController.swift
+++ ToDo-Rev/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var taskTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        taskTable.delegate = self
+        taskTable.dataSource = self
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        var date = cell.viewWithTag(1) as UILabel
+        var name = cell.viewWithTag(2) as UILabel
+        
+        
+        name.text = "b"
+        date.text = "a"
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
     
 }
 
