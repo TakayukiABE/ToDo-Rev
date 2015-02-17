179178a01eac1dddec614462e027aaca128ab5b8
TakayukiABE <s1411344@coins.tsukuba.ac.jp>
2015-02-17 09:18:59 +0900

    add priority picker

--- ToDo-Rev/InputViewController.swift
+++ ToDo-Rev/InputViewController.swift
@@ -21,6 +21,7 @@ class InputViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         picker.dataSource = self
         var month = input.getMonth()
         picker.selectRow(month-1, inComponent: 1, animated: false)
+        picker.selectRow(4, inComponent: 3, animated: false)
     }
     
     
@@ -29,12 +30,13 @@ class InputViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         case 0: return 1000
         case 1: return 12
         case 2: return 30
+        case 3: return 10
         default: return 1
         }
     }
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 3
+        return 4
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
@@ -43,12 +45,23 @@ class InputViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         switch component {
         case 0: return "\(year+row)年"
         case 1: return "\(row+1)月"
-        case 2: return "day日(曜)"
+        case 2: return "33日(曜)"
+        case 3: return "\(row+1)"
         default: break
         }
         
         
         return "a"
+    }
+    
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        switch component {
+        case 0: return 85
+        case 1: return 50
+        case 2: return 95
+        case 3: return 40
+        default: return 0
+        }
     }
     
     
